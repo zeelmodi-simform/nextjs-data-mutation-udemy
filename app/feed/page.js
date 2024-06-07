897e274cd46b830464328e78ac24a6c9a5ee93e7
Zeel Modi <zeel.m@simformsolutions.com>
2024-06-07 15:14:20 +0530

    feat: static and dynamic metadata

--- app/feed/page.js
+++ app/feed/page.js
@@ -1,6 +1,20 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+// export const metadata = {
+//   title: 'Browse all our X posts.',
+//   description: 'Browse our latest post',
+// }
+
+export async function generateMetadata(data) {
+  const posts = await getPosts();
+
+  return {
+    title: `Browse all ${posts.length} our posts.`,
+    description: 'Browse all our posts.'
+  }
+}
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
